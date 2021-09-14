bc9b4f29a1b76fbfa4f4f54499b507b7fff03ae2
unknown <e.u.kornilova@gmail.com>
2021-09-14 11:21:19 +0300

    create app

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const App: React.FC = () => <div>kornilova</div>
+ReactDOM.render(<App />, document.getElementById('root'))
